39e91e212a3b97afa06968af35d981786b010a38
NIVETHA.S <snivu5434@gmail.com>
2026-07-20 21:46:15 +0530

    Add various print and arithmetic operations

--- print.py
+++ print.py
@@ -69,4 +69,110 @@ print("my name is:",name)
 print("my age is: ",age)
 print("my mark is:",mark)
 
+a = int(input())
+b = int(input())
+print(a+b)
+# 5
+# 7
+# 12
+
+a = int(input())
+b = int(input())
+print("sum is ans:", a+b)
+# 10
+# 20
+# sum is ans: 30
+
+print("nivetha")
+# nivetha
+
+print(3)
+# 3
+
+a = 33
+b = 44
+c = a+b
+print(c)
+# 77
+
+print("c")
+a = "335"
+b = "3323"
+print(a+b)
+# 3353323
+
+a = int(4)
+b = int(5)
+c = a+b
+print(c)
+# 9
+
+a = 3
+b = 4
+c = 2
+mul = a*b*c
+add = a+b+c
+l = mul/add
+
+print("mul=", mul)
+print("add=", add)
+print("l=", l)
+# mul= 24
+# add= 9
+# l= 2.6666666666666665
+
+a = 55 % 5
+print(a)
+# 0
+
+b = 55 % 2
+print(b)
+# 1
+
+c = 5 ** 3
+print(c)
+# 125
+
+a = 2 + 4 * 3 + 1
+print(a)
+# 15
+
+z = (2+4) * (3+1)
+print(z)
+# 24
+
+a = input()
+b = input()
+c = a+b
+print(c)
+
+# Hello
+# World
+# HelloWorld
+
+a = 4
+print(type(a))
+# <class 'int'>
+
+a = 20
+a = a+a
+print(a)
+# 40
+
+name = input()
+age = int(input())
+mark = float(input())
+
+print("my name is:", name)
+print("my age is:", age)
+print("my mark is:", mark)
+# Nivetha
+# 19
+# 95.5
+# my name is: Nivetha
+# my age is: 19
+# my mark is: 95.5
+
+
+
 
